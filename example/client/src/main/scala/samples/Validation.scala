3ed1895b11a63c342f4a4228b976bfdbb4191713
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2023-03-17 16:35:12 +0100

    Add CI GitHub workflow

--- example/client/src/main/scala/samples/Validation.scala
+++ example/client/src/main/scala/samples/Validation.scala
@@ -11,9 +11,8 @@ import io.github.iltotore.iron.constraint.all.*
 
 //given Form[IronType[Double, Positive]] = formGen[Double, Positive]
 
-import samples.APositif
 import com.raquo.airstream.state.Var
-case class APositif(
+case class IronSample(
     optional: Option[String],
     optionalInt: Option[Int],
     doublePositive: Double :| GreaterEqual[8.0],
@@ -25,13 +24,13 @@ given IronTypeValidator[Double, GreaterEqual[8.0]] =
     case None         => Left("Not a number")
     case Some(double) => double.refineEither[GreaterEqual[8.0]]
 
-val aPositifVar = Var(APositif(Some("name"), Some(1), 9.1, Some(1)))
+val ironSampleVar = Var(IronSample(Some("name"), Some(1), 9.1, Some(1)))
 
 val validation = div(
-  child <-- aPositifVar.signal.map { item =>
+  child <-- ironSampleVar.signal.map { item =>
     div(
       s"$item"
     )
   },
-  Form.renderVar(aPositifVar)
+  Form.renderVar(ironSampleVar)
 )
