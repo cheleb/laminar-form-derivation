7324bf63c98546fca96eb42f912c2f6dd1e2de42
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2024-09-08 18:25:33 +0200

    feat: Add switchers for different animal types in Sealed.scala

--- examples/client/src/main/scala/samples/Sealed.scala
+++ examples/client/src/main/scala/samples/Sealed.scala
@@ -5,11 +5,14 @@ import dev.cheleb.scalamigen.{*, given}
 import com.raquo.laminar.api.L.*
 
 import com.raquo.airstream.state.Var
+import com.raquo.laminar.nodes.ReactiveHtmlElement
 
 val sealedClasses = {
   sealed trait Animal
+
   case class Horse(name: String, age: Int) extends Animal
   case class Lama(name: String, age: Int, splitDistance: Int) extends Animal
+  case class Otter(name: String, age: Int) extends Animal
 
   case class Owner(name: String, pet: Animal)
 
@@ -17,6 +20,36 @@ val sealedClasses = {
     "Sealed", {
 
       val eitherVar = Var(Owner("Agnes", Horse("Niram <3", 6)))
+
+      case class Switcher(
+          name: String,
+          button: ReactiveHtmlElement[?]
+      )
+
+      object Switcher {
+        def apply[A <: Animal](na: => A): Switcher =
+          val a = na
+          val name = a.getClass.getSimpleName
+          Switcher(
+            name,
+            button(
+              name.filter(_.isLetter),
+              onClick.mapToUnit --> (_ => eitherVar.update(_.copy(pet = a)))
+            )
+          )
+      }
+
+      val switchers = List(
+        Switcher(
+          Horse("Niram", 13)
+        ),
+        Switcher(
+          Lama("Lama", 3, 2)
+        ),
+        Switcher(
+          Otter("Otter", 13)
+        )
+      )
 
       div(
         child <-- eitherVar.signal.map { item =>
@@ -27,23 +60,15 @@ val sealedClasses = {
         child <-- eitherVar.signal
           .distinctByFn((old, nw) => old.pet.getClass == nw.pet.getClass)
           .map { item =>
-            eitherVar.asForm
-          },
-        div(
-          button(
-            "Change to Lama",
-            onClick.mapToUnit --> (_ =>
-              eitherVar.update(_.copy(pet = Lama("Lama", 3, 2)))
+            div(
+              eitherVar.asForm,
+              switchers
+                .filterNot(_.name == eitherVar.now().pet.getClass.getSimpleName)
+                .map(_.button)
             )
-          ),
-          button(
-            "Change to Horse",
-            onClick.mapToUnit --> (_ =>
-              eitherVar.update(_.copy(pet = Horse("Niram", 13)))
-            )
-          )
-        )
+          }
       )
+
     }
   )
 }
