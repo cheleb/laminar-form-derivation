3b88501d1df2bfefd9182fe283c29ba42f331c05
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2024-11-28 21:15:30 +0100

    feat(validation): add case class from string (#244)
    With LatLon case class, we can now validate and parse strings into
    case classes.

--- examples/client/src/main/scala/samples/Validation.scala
+++ examples/client/src/main/scala/samples/Validation.scala
@@ -9,21 +9,54 @@ import io.github.iltotore.iron.constraint.all.*
 
 val validation = {
 
+  case class LatLon(lat: Double, lon: Double) {
+    override def toString: String = s"$lat,$lon"
+  }
+
   given Form[CurrencyCode] = stringForm(CurrencyCode(_))
+
+  given Form[LatLon] = stringFormWithValidation(using
+    new Validator[LatLon] {
+      override def validate(value: String): Either[String, LatLon] = {
+        value.split(",") match {
+          case Array(lat, lon) =>
+            (
+              lat.toDoubleOption.toRight("Invalid latitude"),
+              lon.toDoubleOption.toRight("Invalid longitude")
+            ) match {
+              case (Right(lat), Right(lon)) => Right(LatLon(lat, lon))
+              case (Left(latError), Left(rightError)) =>
+                Left(s"$latError and $rightError")
+              case (Left(latError), _) => Left(latError)
+              case (_, Left(lonError)) => Left(lonError)
+            }
+          case _ => Left("Invalid format")
+        }
+      }
+    }
+  )
 
   case class IronSample(
       curenncyCode: CurrencyCode,
       optional: Option[String],
       optionalInt: Option[Int],
       doubleGreaterThanEight: Double :| GreaterEqual[8.0],
-      optionalDoublePositive: Option[Double :| Positive]
+      optionalDoublePositive: Option[Double :| Positive],
+      latLong: LatLon
   )
 
   given Defaultable[Double :| GreaterEqual[8.0]] with
     def default: Double :| GreaterEqual[8.0] = 8.0
 
   val ironSampleVar = Var(
-    IronSample(CurrencyCode("Eur"), Some("name"), Some(1), 9.1, Some(1))
+    IronSample(
+      CurrencyCode("Eur"),
+      Some("name"),
+      Some(1),
+      9.1,
+      Some(1),
+      LatLon(1, 2)
+    )
   )
 
   val errorBus = ironSampleVar.errorBus
