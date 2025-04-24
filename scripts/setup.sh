19bd3a59777527f9acd20cc096fa38e9ff46c55a
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2025-04-24 11:42:03 +0200

    fix: refactor get_mtime function for cross-platform compatibility in … (#297)
    * fix: refactor get_mtime function for cross-platform compatibility in setup script
    
    * fix: update vite version to ^6.3.3 and add new dependencies in package-lock.json

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,16 +8,23 @@ filename_lock=node_modules/.package-lock.json
 
 cd examples/client
 
+# Define get_mtime function based on OS
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS
+    get_mtime() { stat -f %m "$1"; }
+else
+    # Linux and others
+    get_mtime() { stat -c %Y "$1"; }
+fi
+
 if [ ! -f "$filename_lock" ]; then
     echo "First time setup: Installing npm dependencies..."
-
     npm i
 else
     npmRefresh=3600
     filename=package.json
-    age=$(($(date +%s) - $(stat -t %s -f %m -- "$filename")))
-    age_lock=$(($(date +%s) - $(stat -t %s -f %m -- "$filename_lock")))
-#    if [ $age_lock -gt $npmRefresh ] || [ $age_lock -gt $age ]; then
+    age=$(($(date +%s) - $(get_mtime "$filename")))
+    age_lock=$(($(date +%s) - $(get_mtime "$filename_lock")))
     if [ $age_lock -gt $age ]; then
         echo "Reinstalling npm dependencies..."
         npm i
